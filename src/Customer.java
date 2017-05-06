42113c019659ff50d3fcf8d73a629235746f7c20
Sandra Kramlich <sandra.kramlich@t-online.de>
2017-05-06 15:13:06 +0200

    STEP 6: Extract Function getFrequentRenterPoints(int points, Rental each)

--- src/Customer.java
+++ src/Customer.java
@@ -25,11 +25,7 @@ class Customer {
 
 		while (enum_rentals.hasMoreElements()) {
 			Rental each = (Rental) enum_rentals.nextElement();
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
 			// show figures for this rental
 			resultStatement += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
@@ -39,6 +35,15 @@ class Customer {
 		resultStatement += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		resultStatement += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return resultStatement;
+	}
+
+	private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
+		// add frequent renter points
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 
 }
