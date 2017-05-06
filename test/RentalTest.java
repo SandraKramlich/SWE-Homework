63be8fe23795d3562dce396169b80a1943b72fb2
Sandra Kramlich <sandra.kramlich@t-online.de>
2017-05-06 15:30:08 +0200

    STEP 8: Added test for getFrequentRenterPoints(); Method

--- test/RentalTest.java
+++ test/RentalTest.java
@@ -6,27 +6,41 @@ import org.junit.Test;
 public class RentalTest {
 
     private Rental r1;
+    private Rental r2;
     private Movie m1;
-    private double EXPECTED = 15.0;
+    private Movie m2;
+    private double EXPECTED1 = 15.0;
+    private double EXPECTED2 = 12.0;
 
     @Before
     public void setup() {
         m1 = new Movie("movie1", 1);
+        m2 = new Movie("movie2", 2);
         r1 = new Rental(m1, 5);
+        r2 = new Rental(m2, 10);
     }
 
     @Test
     public void testSettingDaysRented() throws Exception {
         assertEquals(5, r1.getDaysRented());
+        assertEquals(10, r2.getDaysRented());
     }
 
     @Test
     public void testSettingMovie() throws Exception {
         assertEquals(m1, r1.getMovie());
+        assertEquals(m2, r2.getMovie());
     }
 
 	@Test
     public void testAmountCalculation() throws Exception{
-    	assertEquals(EXPECTED, r1.getCharge(),0);
+    	assertEquals(EXPECTED1, r1.getCharge(),0);
+    	assertEquals(EXPECTED2, r2.getCharge(),0);
+    }
+	
+	@Test
+    public void testFrequentRenterPoints() throws Exception{
+    	assertEquals(2, r1.getFrequentRenterPoints(),0);
+    	assertEquals(1, r2.getFrequentRenterPoints(),0);
     }
 }
