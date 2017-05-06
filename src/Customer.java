a468004503e8f04d5793625869ffb645a4a8220c
Sandra Kramlich <sandra.kramlich@t-online.de>
2017-05-06 15:41:49 +0200

    STEP 10: Replace Temp with Query (frequentRenterPoints with getTotalFrequentRenterPoints())

--- src/Customer.java
+++ src/Customer.java
@@ -17,22 +17,30 @@ class Customer {
 	};
 
 	public String statement() {
-		int frequentRenterPoints = 0;
 		Enumeration enum_rentals = this.rentals.elements();
 		String resultStatement = "Rental Record for " + this.getName() + "\n";
 		resultStatement += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
 		while (enum_rentals.hasMoreElements()) {
 			Rental each = (Rental) enum_rentals.nextElement();
-			frequentRenterPoints += each.getFrequentRenterPoints();
 			// show figures for this rental
 			resultStatement += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
 		}
 		// add footer lines
 		resultStatement += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-		resultStatement += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		resultStatement += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
 		return resultStatement;
+	}
+
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		Enumeration rentals = this.rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
 	}
 
 	private double getTotalCharge() {
